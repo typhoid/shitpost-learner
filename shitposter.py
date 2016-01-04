276d74c4c1038ee0873009872116ca52aa8ad109
Chad Miller <cmiller86@gatech.edu>
2016-01-03 21:09:07 -0600

    Web version

--- shitposter.py
+++ shitposter.py
@@ -9,23 +9,7 @@ from HTMLParser import HTMLParser
 from pymarkovchain import MarkovChain
 from random import randint
 
-html_rep = {
-    '&amp;' : '&',
-    '&quot;' : '"',
-    '&#039;' : "'",
-    '&gt;' : '>',
-    '&lt;' : '<'
-}
-
-def sanitize( com ):
-    retval = re.sub( r'\<.+\>', '', com )
-    
-    for sym in html_rep:
-        retval = retval.replace( sym, html_rep[sym] )
-    
-    return retval
-
-def thread_prop( images, board, thread_id ):
+def image_prop( images, board, thread_id ):
     try:
         response = urllib2.urlopen( 'http://a.4cdn.org' + board + 'thread/' + str( thread_id ) + '.json' )
     except ( urllib2.HTTPError ):
@@ -34,29 +18,20 @@ def thread_prop( images, board, thread_id ):
     data = json.loads( response.read() )
 
     for post in data['posts']:
-        if 'com' in post:
-            sanitized = sanitize( post['com'] )
-            retval = u' {}'.format( sanitized )
-        
         if 'filename' in post:
             images.append( str( post['tim'] ) + post['ext'] )
-    
-    return retval
 
-def analyze_board( mc, board ):
-    train_string = u''
+def get_board_images( board ):
     images = []
 
-    print( 'Training... (may take a while)' )
+    print( 'Gathering images... (may take a while)' )
     response = urllib2.urlopen( 'http://a.4cdn.org' + board + 'threads.json' )
     data = json.loads( response.read() )
-    thread_ids = list()
 
     for page in data:
         for thread in page['threads']:
-            train_string += thread_prop( images, board, thread['no'] )
+            image_prop( images, board, thread['no'] )
 
-    mc.generateDatabase( train_string )
     return images
 
 def image_grab( images, board ):
@@ -73,28 +48,8 @@ def shitpost_loop( mc, images, board ):
         if read == '?':
             print( 'Hit enter to generate a shitpost.' )
             print( "Enter 'exit' to exit the program." )
-            print( "Enter 'train' to re-train the shitposter (this takes a while)." )
-            print( "Enter 'board <board>' to switch to a different board (this takes a while)." )
-        elif read == 'train':
-            images = analyze_board( mc, board )
-            print( 'Re-training complete.' )
         elif read == 'exit':
             pass
-        elif read.startswith( 'board' ):
-            try:
-                board = read.split()[1]
-            except ( IndexError ):
-                print( 'No board specified.' )
-                continue
-
-            try:
-                urllib2.urlopen( 'http://a.4cdn.org' + board + 'threads.json' )
-            except ( urllib2.HTTPError ):
-                print( 'Invalid board.' )
-                continue
-
-            images = analyze_board( mc, board )
-            print( 'Switched to {}.'.format( board ) )
         elif read:
             print( 'Invalid input.' )
         else:
@@ -105,7 +60,6 @@ def shitpost_loop( mc, images, board ):
             print( u'>{}'.format( shitpost ) )
 
 def main( args ):
-    mc = MarkovChain( './shitpost_data' )
     board = ''
 
     print( 'Enter the name of the board you would like to learn how to shitpost from.' )
@@ -120,7 +74,8 @@ def main( args ):
             print( "Invalid board - try again." )
             board = ''
 
-    images = analyze_board( mc, board )
+    mc = MarkovChain( './shitpost_data_{}'.format( board[1:-1] ) )
+    images = get_board_images( board )
     shitpost_loop( mc, images, board )
 
 if __name__ == '__main__':
